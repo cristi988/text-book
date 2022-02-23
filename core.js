73b0f238dbe7f11a75fcd5527672e4cbbcb301b3
Cristian Cojita <cristi.gabriel988@gmail.com>
2022-02-23 11:20:39 +0000

    added validation

--- core.js
+++ core.js
@@ -3,39 +3,92 @@ let store = [];
 
 let template = ``;
 
+/**
+ * This function gets the data from inputs 
+ * 
+ */
 function getData(){
 
-    let fName = document.getElementById('fName').value;
-    let lName = document.getElementById('lName').value;
-    let address = document.getElementById('address').value;
+    //collect data 
 
-    store.push({fName : fName, lName : lName, address : address});
+    let fName = document.getElementById('fName');
+    let lName = document.getElementById('lName');
+    let email = document.getElementById('email');
+    let address = document.getElementById('address');
+    let phoneNumber = document.getElementById('phone-number');
+    //validate 
+    validate([fName, lName, email, address, phoneNumber]);
+
+    store.push({fName: fName.value, lName : lName.value, address : address.value, email : email.value, phoneNumber : phoneNumber.value});
 
     render()
-
-    console.log(fName, lName, address);
 }
 
+/**
+ * This function creates the template and injects data
+ * 
+ */
 function render() {
 
-    let container = document.getElementById('text_book');
+    let container = document.querySelector('.text-book-content');
 
     template = ``;
 
     for(i = 0; i < store.length; i++){
         console.log(i)
         store[i];
-        template += `${store[i].fName} - ${store[i].lName} - ${store[i].address} - <button onclick='remove(${i})'>Delete</button> <br>`;
-
+        template += `${store[i].fName} - ${store[i].lName} - ${store[i].address} - ${store[i].email} - ${store[i].phoneNumber} - 
+        <i onclick="remove(${i})" class="text-danger bi bi-x-square-fill"></i> <br>`;
     }
 
     container.innerHTML = template;
 }
 
+
+/**
+ * This function deletes an object from the store array
+ * 
+ * @param {*} index 
+ */
 function remove(index){
 
    store = store.filter( (item, key) =>  key != index);
    render();
 
 }
+
+/**
+ * This function validates if the fields(inputs) are empty 
+ * 
+ * @param {*} field 
+ * @returns 
+ */
+function isValid(field) {
+
+    if (field == '' || field.length < 1) {
+        return false;
+    }
+
+    return !!field;
+}
+
+function renderFeedback(element) {
+
+    if (isValid(element.value)){
+        element.classList.add('border-success');
+        element.classList.remove('border-danger');
+    } else{
+        element.classList.add('border-danger');
+        element.classList.remove('border-success');
+        throw new Error('You must fill all fields')
+    }
+
+}
+
+function validate(data) {
+    for (let i = 0; i < data.length; i++) {
+        renderFeedback(data[i]);
+    }
+}
+
 
