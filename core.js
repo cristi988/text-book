cea87c4d99340aecb3b3b7501f7901ce80faab3d
Cristian Cojita <cristi.gabriel988@gmail.com>
2022-02-24 09:27:54 +0000

    finished valdiation feedback

--- core.js
+++ core.js
@@ -3,6 +3,7 @@ let store = [];
 
 let template = ``;
 
+
 /**
  * This function gets the data from inputs 
  * 
@@ -33,18 +34,10 @@ function render() {
 
     let container = document.querySelector('.text-book-content');
 
-    template = ``;
+    template = '';
 
-    for(i = 0; i < store.length; i++){
-        console.log(i)
-        store[i];
-        template += `<div class="main-template"> <div class="template">${store[i].fName}  ${store[i].lName}</div> <div class="template">${store[i].address}</div>
-        <div class="template">${store[i].email}</div> <div class="template">${store[i].phoneNumber}</div> 
-        <div class="template"><i onclick="remove(${i})" class="text-danger bi bi-x-square-fill"></i></div> <br> </div>`;
-
-        // template += `<table> <tr > <td>${store[i].fName} ${store[i].lName} <i onclick="remove(${i})" class="text-danger bi bi-x-square-fill"></i></td></tr>  <tr><td>${store[i].address}</td></tr>  
-        // <tr><td>${store[i].email}</td></tr>  <tr><td>${store[i].phoneNumber}</td></tr></table>`;
-        
+    for(i = 0; i < store.length; i++){    
+        cardTemplate(i);
     }
 
     container.innerHTML = template;
@@ -78,27 +71,52 @@ function isValid(field) {
     return !!field;
 }
 
+/**
+ * This function checks if the fields are empty or completed
+ * 
+ * @param {*} element 
+ */
 function renderFeedback(element) {
+
+    let errorSpan = document.querySelector(`.error-feedback[data-id = ${element.id}]`);
 
     if (isValid(element.value)){
         element.classList.add('border-success');
         element.classList.remove('border-danger');
+        errorSpan.classList.add('invisible');
     } else{
         element.classList.add('border-danger');
         element.classList.remove('border-success'); 
-
-        document.querySelector('#error').classList.remove('error-danger');
+       
+       errorSpan.classList.remove('invisible');
 
         throw new Error('You must fill all fields')
     }
 
 }
 
-
+/**
+ * This function validates data
+ * 
+ * @param {*} data 
+ */
 function validate(data) {
     for (let i = 0; i < data.length; i++) {
         renderFeedback(data[i]);
     }
+}
+
+/**
+ * This function creates the template 
+ *  
+ * @param {*} i 
+ */
+function cardTemplate(i) {
+
+        template += `<div class="main-template"> <div class="template">${store[i].fName}  ${store[i].lName}</div> <div class="template">${store[i].address}</div>
+        <div class="template">${store[i].email}</div> <div class="template">${store[i].phoneNumber}</div> 
+        <div class="template"><i onclick="remove(${i})" class="text-danger bi bi-x-square-fill"></i></div> <br> </div>`;
+
 }
 
 
